f2bbdfc4762848abd275f88a550e5e314972e5d3
Marcos Lopes <marcos.eddul@gmail.com>
2021-12-01 20:01:54 -0300

    sendo changes

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <p>welcome to react</p>
+            </div>
+        );
+    }
+}
+
+export default Hello;
