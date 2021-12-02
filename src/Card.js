d938937234eaf1851204db6ec50eae2fab81a21b
Marcos Lopes <marcos.eddul@gmail.com>
2021-12-01 22:02:42 -0300

    ok

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const card = () => {
+const Card = (props) => {
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src="https://robohash.org/marcos"/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     );
 }
 
-export default card;
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import card from './card';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { robots } from './robots';
+
 ReactDOM.render(
   <div>
-    <card id={robots[0].id} name={robots[0].name} email={robots[0]}/>
-    <card id={robots[1].id} name={robots[1].name} email={robots[1]}/>
-    <card id={robots[2].id} name={robots[2].name} email={robots[2]}/>
-    <card id={robots[3].id} name={robots[3].name} email={robots[3]}/>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0]}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1]}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2]}/>
+    <Card id={robots[3].id} name={robots[3].name} email={robots[3]}/>
   </div>,
   document.getElementById('root')
 );
