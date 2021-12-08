12497069f806841020697755ab7afa29ceb03933
Marcos Lopes <marcos.eddul@gmail.com>
2021-12-07 22:43:39 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
+
 ReactDOM.render(
   <App />
   ,document.getElementById('root')
@@ -13,4 +14,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-npmreportWebVitals();
+reportWebVitals();
